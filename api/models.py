42bc8a1465782e6bfda78463c5c48de2864656ce
Ezequiel Salas <ezjava@gmail.com>
2019-04-13 13:28:33 -0400

    Adding Model

--- api/models.py
+++ api/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+
+class Author(models.Model):
+    name = models.CharField(max_length=250)
+
+
+class Book(models.Model):
+    name = models.CharField(max_length=250)
+    isbn = models.CharField(max_length=50)
+    authors = models.ManyToManyField(Author, through='BookAuthor', through_fields=('author', 'book'),)
+    number_of_pages = models.IntegerField()
+    publisher = models.CharField(max_length=250)
+    country = models.CharField(max_length=250)
+    release_date = models.DateField()
+
+
+class BookAuthor(models.Model):
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+
